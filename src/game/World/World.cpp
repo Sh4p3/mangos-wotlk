13833b925c6aba226b0a00b71ab2c4d867c472da
David <davidparraausina@gmail.com>
2024-01-28 14:29:28 +0000

    Move the immersive config init into immersive init method

--- src/game/World/World.cpp
+++ src/game/World/World.cpp
@@ -90,7 +90,6 @@
 #endif
 
 #ifdef ENABLE_IMMERSIVE
-#include "ImmersiveConfig.h"
 #include "Immersive.h"
 #endif
 
@@ -1798,10 +1797,6 @@ void World::SetInitialWorldSettings()
 
 #ifdef ENABLE_PLAYERBOTS
     sPlayerbotAIConfig.Initialize();
-#endif
-
-#ifdef ENABLE_IMMERSIVE
-    sImmersiveConfig.Initialize();
 #endif
 
     sTransmogrification->LoadConfig(false);
