eeb1b8ef421aabb43f09ef76e5ccd50a7503e6be
David Parra <davidparraausina@gmail.com>
2024-02-01 10:17:54 +0000

    Ike3 bots immersive mod update
    * Move immersive mod into a separate module
    
    * Move the immersive config init into immersive init method
    
    * Update immersive methods and move more logic into module

--- src/game/World/World.cpp
+++ src/game/World/World.cpp
@@ -90,7 +90,7 @@
 #endif
 
 #ifdef ENABLE_IMMERSIVE
-#include "Immersive.h"
+#include "ImmersiveMgr.h"
 #endif
 
 #include <algorithm>
@@ -1219,7 +1219,7 @@ void World::SetInitialWorldSettings()
     sObjectMgr.LoadSQLDBCs();
 
 #ifdef ENABLE_IMMERSIVE
-    sImmersive.Init();
+    sImmersiveMgr.Init();
 #endif
 
     // Load before npc_text, gossip_menu_option, script_texts
@@ -1994,7 +1994,7 @@ void World::Update(uint32 diff)
 #endif
 
 #ifdef ENABLE_IMMERSIVE
-    sImmersive.Update(diff);
+    sImmersiveMgr.Update(diff);
 #endif
 
     /// <li> Handle session updates
