63f2fef69fc1bf3c2c12ca7ec593b88853867eb4
celguar <celguar@gmail.com>
2024-05-12 23:14:12 +0300

    Fix merge

--- src/game/Server/WorldSession.cpp
+++ src/game/Server/WorldSession.cpp
@@ -640,7 +640,7 @@ void WorldSession::LogoutPlayer()
             _player->GetPlayerbotMgr()->LogoutAllBots(true);
 #endif
 #ifdef ENABLE_PLAYERBOTS
-        sLog.outChar("Account: %d (IP: %s) Logout Character:[%s] (guid: %u)", GetAccountId(), m_Socket ? GetRemoteAddress().c_str() : "bot", _player->GetName(), _player->GetGUIDLow());
+        sLog.outChar("Account: %d (IP: %s) Logout Character:[%s] (guid: %u)", GetAccountId(), m_socket ? GetRemoteAddress().c_str() : "bot", _player->GetName(), _player->GetGUIDLow());
 #else
         sLog.outChar("Account: %d (IP: %s) Logout Character:[%s] (guid: %u)", GetAccountId(), GetRemoteAddress().c_str(), _player->GetName(), _player->GetGUIDLow());
 #endif
